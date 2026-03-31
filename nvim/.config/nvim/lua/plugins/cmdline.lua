f8731b49c6d4a171cad1f535bb2800794ad34989
MoobyMoo <asianboiow@gmail.com>
2026-04-01 01:10:45 +0800

    Disable fine-cmdline plugin

--- nvim/.config/nvim/lua/plugins/cmdline.lua
+++ nvim/.config/nvim/lua/plugins/cmdline.lua
@@ -1,15 +1,4 @@
 return {
   "VonHeikemen/fine-cmdline.nvim",
-  lazy = false,
-  dependencies = { "MunifTanjim/nui.nvim" },
-  config = function()
-    require("fine-cmdline").setup({
-      cmdline = {
-        enable_keymaps = true,
-      },
-    })
-    
-    -- Keybindings for fine-cmdline
-    vim.keymap.set("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
-  end,
+  enabled = false,
 }
